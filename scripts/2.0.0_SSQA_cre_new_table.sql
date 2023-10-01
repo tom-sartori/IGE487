15310d3484aaf4269343d4850dd7ebbbbb4bdb03
AlexTheisse <99749361+AlexTheisse@users.noreply.github.com>
2023-10-01 12:49:45 -0300

    [+]Début implémentation nouvelle tables

---
+++ scripts/2.0.0_SSQA_cre_new_table.sql
@@ -0,0 +1,45 @@
+set schema 'SSQA';
+
+-- Hors service
+create table HorsService (
+    idStation integer not null,
+    dateDebut date not null,
+    dateFin date not null,
+    motif integer not null,
+    primary key (idStation, dateDebut, motif),
+    foreign key (idStation) references Station(idStation),
+    foreign key (motif) references nature_Hors_service(idMotif)
+);
+-- Nature Hors service
+create table nature_Hors_service (
+    idMotif integer not null,
+    nom varchar(50) not null,
+    primary key (idMotif)
+);
+-- Station nom
+create table Station_nom (
+    idStation integer not null,
+    nom varchar(50) not null,
+    primary key (idStation),
+    foreign key (idStation) references Station(idStation)
+);
+-- Erreur mesure
+create table erreur_mesure (
+    station integer not null,
+    moment date not null,
+    variable integer not null,
+    erreur_mesure_code integer not null,
+    primary key (station, moment, variable, erreur_mesure_code),
+    foreign key (station) references Station(idStation),
+    
+    foreign key (variable) references Variable(idVariable),
+    foreign key (erreur_mesure_code) references erreur_mesure_code(code)    
+)
+;
+-- Erreur mesure code
+create table erreur_mesure_code (
+    code integer not null,
+    nom varchar(50) not null,
+    primary key (code)
+);
+
