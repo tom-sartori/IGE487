9f9c2a4dfef85dbb636c038bb217114fdf7068ba
BigBaz <louisvincent@famillecapelli.fr>
2023-10-01 22:20:10 -0400

    [+] R06 sans tests

--- scripts/2.0.0_SSQA_R06.sql
+++ scripts/2.0.0_SSQA_R06.sql
@@ -1,26 +1,32 @@
 set schema 'SSQA';
---Ajout attribut debut_service et fin_service dans la table station
-alter table station add column debut_service date not null;
-alter table station add column fin_service date ;
---ajout contrainte de debut_service et fin_service
-alter table station add constraint Station_cc0 check (fin_service is null or fin_service > debut_service);
-create domains Motif_code 
+
+--cAjout attribut debut_service et fin_service dans la table station
+alter table Station add column debut_service Estampille;
+alter table Station add column fin_service Estampille;
+
+-- Ajout contrainte de debut_service et fin_service
+alter table Station add constraint Station_date_service check (debut_service is null or fin_service is null or fin_service >= debut_service);
+
+create domain Hors_service_code 
 as integer not null;
+
+create domain Description_hors_service
+as varchar(128) not null;
+
+create table Nature_Hors_service (
+    code Hors_service_code not null,
+    description Description_hors_service not null,
+    constraint Nature_hors_service_cc0 primary key (code)
+);
+
 create table Hors_service (
     station Station_Code not null,
     debut Estampille not null,
     fin Estampille not null,
-    nature Motif_code not null,
-   constraint Hors_service_cc0 primary key (station, debut,nature),
-    foreign key (idStation) references Station(code),
-    foreign key (nature) references nature_Hors_service(code)
-    constraint Hors_service_cc1 check (fin > debut)
-
-);
-
-create table Nature_Hors_service (
-    code Motif_code not null,
-    description varchar(50) not null,
-    constraint Nature_hors_service_cc0 primary key (code)
-
+    nature Hors_service_code not null,
+   
+    constraint Hors_service_cc0 primary key (station, debut, nature),
+    constraint Hors_service_cr0 foreign key (station) references Station(code),
+    constraint Hors_service_cr1 foreign key (nature) references Nature_Hors_service(code),
+    constraint Hors_service_dates_valides check (fin >= debut)
 );
