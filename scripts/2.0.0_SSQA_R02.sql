844b3817980924b0f6090eb4cd4053d1744b193f
tom-sartori <60385058+tom-sartori@users.noreply.github.com>
2023-10-01 22:03:00 -0400

    Add comments.

--- scripts/2.0.0_SSQA_R02.sql
+++ scripts/2.0.0_SSQA_R02.sql
@@ -1,5 +1,38 @@
+/*
+-- =========================================================================== A
+-- SSQA_R02.sql
+-- ---------------------------------------------------------------------------
+Activité : IFT187_2023-1
+Encodage : UTF-8, sans BOM; fin de ligne Unix (LF)
+Plateforme : PostgreSQL 12 à 15
+Responsable : alexandre.theisse@usherbrooke.ca, louis-vincent.capelli@usherbrooke.ca, raphael.turcotte2@usherbrooke.ca, tom.sartori@usherbrooke.ca,
+Version : 1.0.0
+Statut : en vigueur
+Résumé : Contraindre plus strictement les symboles.
+-- =========================================================================== A
+*/
+
+-- Définition du schéma
+create schema if not exists "SSQA";
 set schema 'SSQA';
 
 -- Selon moi, rien à mettre ici, les seules règles que j'ai trouvé sont des règles d'utilisation plutôt que de création
 -- en plus, il est possible que l'uutilisateur ait à créer son propre symbole d'unité,
 -- par exemple kg/m3, donc je ne vois pas comment on pourrait mettre des règles ici
+
+/*
+-- =========================================================================== Z
+Contributeurs :
+    capl1101 louis-vincent.capelli@usherbrooke.ca
+    sart0701 tom.sartori@usherbrooke.ca
+    thea1804 alexandre.theisse@usherbrooke.ca
+    turr3004 raphael.turcotte2@usherbrooke.ca
+
+Tâches réalisées :
+  2023-10-01 (capl1101, sart0701, thea1804, turr3004) : Etude du problème. Solution réalisée par la R01.
+
+
+-- -----------------------------------------------------------------------------
+-- SSQA_RO1.sql
+-- =========================================================================== Z
+*/
--- scripts/2.0.0_SSQA_R04.sql
+++ scripts/2.0.0_SSQA_R04.sql
@@ -1,3 +1,19 @@
+/*
+-- =========================================================================== A
+-- SSQA_R04.sql
+-- ---------------------------------------------------------------------------
+Activité : IFT187_2023-1
+Encodage : UTF-8, sans BOM; fin de ligne Unix (LF)
+Plateforme : PostgreSQL 12 à 15
+Responsable : alexandre.theisse@usherbrooke.ca, louis-vincent.capelli@usherbrooke.ca, raphael.turcotte2@usherbrooke.ca, tom.sartori@usherbrooke.ca,
+Version : 1.0.0
+Statut : en vigueur
+Résumé : Différentes valeurs et intervalles de validité sont associés à la définition et l’exploitation des variables. Il est nécessaire de s’assurer que leur cohérence.
+-- =========================================================================== A
+*/
+
+-- Définition du schéma
+create schema if not exists "SSQA";
 set schema 'SSQA';
 
 -- Ajout contrainte pour la table variable
@@ -26,3 +42,20 @@ end;
 $$ language plpgsql;
 
 create trigger valider_exigence_trigger before insert or update on exigence for each row execute function valider_exigence();
+
+/*
+-- =========================================================================== Z
+Contributeurs :
+    capl1101 louis-vincent.capelli@usherbrooke.ca
+    sart0701 tom.sartori@usherbrooke.ca
+    thea1804 alexandre.theisse@usherbrooke.ca
+    turr3004 raphael.turcotte2@usherbrooke.ca
+
+Tâches réalisées :
+  2023-10-01 (capl1101, sart0701, thea1804, turr3004) : Vérifier que la valeur de référence de la variable est comprise dans l’intervalle de validité.
+  2023-10-01 (capl1101, sart0701, thea1804, turr3004) : Vérifier que les min et max des exigences sont compris dans l’intervalle de validité.
+
+-- -----------------------------------------------------------------------------
+-- SSQA_RO4.sql
+-- =========================================================================== Z
+*/
