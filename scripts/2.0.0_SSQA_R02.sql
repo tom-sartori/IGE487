3cdcf68ca1a304684296035bd4fc056af7023eb0
BigBaz <louisvincent@famillecapelli.fr>
2023-10-01 20:14:59 -0400

    [+] Commentaire R02

--- scripts/2.0.0_SSQA_R02.sql
+++ scripts/2.0.0_SSQA_R02.sql
@@ -1,1 +1,5 @@
 set schema 'SSQA';
+
+-- Selon moi, rien à mettre ici, les seules règles que j'ai trouvé sont des règles d'utilisation plutôt que de création
+-- en plus, il est possible que l'uutilisateur ait à créer son propre symbole d'unité,
+-- par exemple kg/m3, donc je ne vois pas comment on pourrait mettre des règles ici
