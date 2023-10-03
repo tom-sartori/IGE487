1e7a57b390e4670b60a80f320d992b20ea546b70
BigBaz <louisvincent@famillecapelli.fr>
2023-10-02 23:42:57 -0400

    [~] fix ins_val R05

--- scripts/2.0.1_SSQA_R05_ins-val.sql
+++ scripts/2.0.1_SSQA_R05_ins-val.sql
@@ -18,7 +18,7 @@ set schema 'SSQA';
 
 insert into Unite (nom, sym, mult, add) values ('Kelvin', 'Ktest', 1, 0);
 
-insert into Variable (code, nom, unite, valref, methode) values ('T', 'Température', 'Ktest', 273.15, 1);
+insert into Variable (code, nom, unite, valref, methode) values ('T', 'Température', 'Ktest', 273.15, 'Moyenne horaire');
 
 /*
 -- =========================================================================== Z
