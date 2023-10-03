8d79069a9657809bab9fcbb0b518a2c8ee0303fc
BigBaz <louisvincent@famillecapelli.fr>
2023-10-03 02:10:13 -0400

    [+] ins-inv R10

--- scripts/2.0.2_SSQA_R10_ins-inv.sql
+++ scripts/2.0.2_SSQA_R10_ins-inv.sql
@@ -8,7 +8,7 @@ Plateforme : PostgreSQL 12 à 15
 Responsable : alexandre.theisse@usherbrooke.ca, louis-vincent.capelli@usherbrooke.ca, raphael.turcotte2@usherbrooke.ca, tom.sartori@usherbrooke.ca,
 Version : 1.0.0
 Statut : en vigueur
-Résumé : Tentatives erronées d'insertion de valeurs dans les tables.
+Résumé : Tentatives erronées d'insertion de valeurs dans les tables Erreur_mesure_code et Erreur_mesure et Mesure.
 -- =========================================================================== A
 */
 
@@ -16,7 +16,46 @@ Résumé : Tentatives erronées d'insertion de valeurs dans les tables.
 create schema if not exists "SSQA";
 set schema 'SSQA';
 
+/*
+Tentative d’insertion des données invalides à des fins de tests unitaires pour les tables
+du schéma SSQA (système de surveillance de la qualité de l’air). Toutes les insertions
+doivent entraîner une échec.
 
+On suppose que les données valides du fichier SSQA_ins-val.sql correspondant ont été insérées
+au préalable.
+*/
+
+-- code null
+insert into Erreur_mesure_code (code, nom) values 
+  (null, 'Capteur défectueux');
+
+-- code non-unique
+insert into Erreur_mesure_code (code, nom) values 
+  (1, 'Capteur défectueux');
+
+-- nom null
+insert into Erreur_mesure_code (code, nom) values 
+  (3, null);
+
+-- code mauvais type
+insert into Erreur_mesure_code (code, nom) values 
+  ('test', 'osef');
+
+-- nom trop long
+insert into Erreur_mesure_code (code, nom) values 
+  (3, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl nec tincidunt ultricies, nunc nisl aliquam nunc, sit amet aliquam nisl nunc vitae nisl. Donec auctor, nisl nec tincidunt ultricies, nunc nisl aliquam nunc, sit amet aliquam nisl nunc vitae nisl. Donec auctor, nisl nec tincidunt ultricies, nunc nisl aliquam nunc, sit amet aliquam nisl nunc vitae nisl. Donec auctor, nisl nec tincidunt ultricies, nunc nisl aliquam nunc, sit amet aliquam nisl nunc vitae nisl. Donec auctor, nisl nec tincidunt ultricies, nunc nisl aliquam nunc, sit amet aliquam nisl nunc vitae nisl. Donec auctor, nisl nec tincidunt ultricies, nunc nisl aliquam nunc, sit amet aliquam nisl nunc vitae nisl.');
+
+-- code non-existant
+insert into Erreur_mesure (station, moment, variable, erreur_mesure_code) values
+  ('10000', '2021-01-01 00:00:01', 'CO', 3);
+
+-- mesure non-existante
+insert into Erreur_mesure (station, moment, variable, erreur_mesure_code) values
+  ('10000', '2021-01-01 00:00:03', 'CO', 1);
+
+-- colonne valide n'exsite plus
+insert into Mesure (station, moment, variable, valeur, valide) values
+  ('10000', '2021-01-01 00:00:03', 'CO', 50, true);
 
 /*
 -- =========================================================================== Z
