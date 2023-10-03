8f220ff77ac7d44a070b85915fa7e937646481ff
BigBaz <louisvincent@famillecapelli.fr>
2023-10-03 00:08:51 -0400

    [+] ins-val R06

--- scripts/2.0.1_SSQA_R06_ins-val.sql
+++ scripts/2.0.1_SSQA_R06_ins-val.sql
@@ -8,7 +8,7 @@ Plateforme : PostgreSQL 12 à 15
 Responsable : alexandre.theisse@usherbrooke.ca, louis-vincent.capelli@usherbrooke.ca, raphael.turcotte2@usherbrooke.ca, tom.sartori@usherbrooke.ca,
 Version : 1.0.0
 Statut : en vigueur
-Résumé : Insertion de valeurs dans les tables.
+Résumé : Insertion de valeurs dans les tables Station, Nature_hors_service et Hors_service.
 -- =========================================================================== A
 */
 
@@ -16,7 +16,25 @@ Résumé : Insertion de valeurs dans les tables.
 create schema if not exists "SSQA";
 set schema 'SSQA';
 
+-- Insertion de valeurs dans la table Station
+insert into Station (code, nom, longitude, latitude, altitude, debut_service, fin_service) values
+('000001', 'Station 1', 45.378, -71.925, 200, '2021-01-01', null),
+('000002', 'Station 2', 45.378, -71.925, 200, '2021-01-01', null),
+('000003', 'Station 3', 45.378, -71.925, 200, '2021-01-01', null);
 
+-- Insertion de valeurs dans la table Nature_hors_service
+insert into Nature_hors_service (code, description) values
+(-1, 'Inconnu'),
+(1, 'Maintenance'),
+(2, 'Calibration'),
+(3, 'Réparation'),
+(4, 'Autre');
+
+-- Insertion de valeurs dans la table Hors_service
+insert into Hors_service (station, debut, fin, nature) values
+('000001', '2021-01-01', '2021-01-02', 1),
+('000002', '2021-01-01', '2021-01-02', 2),
+('000003', '2021-01-01', '2021-01-02', 3);
 
 /*
 -- =========================================================================== Z
