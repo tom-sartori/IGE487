a601ac24fea8daba981175386d93cbde69a4c77b
AlexTheisse <99749361+AlexTheisse@users.noreply.github.com>
2023-10-29 21:19:51 -0300

    [~] Ajout Contributeurs

---
+++ scripts/3.4.0_SSQA_EMIR_Variable.sql
@@ -0,0 +1,33 @@
+/*
+-- =========================================================================== A
+-- 3.4.0_SSQA_EMIR_Variable.sql
+-- ---------------------------------------------------------------------------
+Activité : IFT187_2023-1
+Encodage : UTF-8, sans BOM; fin de ligne Unix (LF)
+Plateforme : PostgreSQL 12 à 15
+Responsable : alexandre.theisse@usherbrooke.ca, louis-vincent.capelli@usherbrooke.ca, raphael.turcotte2@usherbrooke.ca, tom.sartori@usherbrooke.ca,
+Version : 1.0.0
+Statut : en vigueur
+Résumé : Ajout des procédures et fonctions EMIR pour les stations.
+-- =========================================================================== A
+*/
+
+-- Définition du schéma
+create schema if not exists "SSQA_PUB";
+set schema 'SSQA_PUB';
+
+/*
+-- =========================================================================== Z
+Contributeurs :
+    capl1101 louis-vincent.capelli@usherbrooke.ca
+    sart0701 tom.sartori@usherbrooke.ca
+    thea1804 alexandre.theisse@usherbrooke.ca
+    turr3004 raphael.turcotte2@usherbrooke.ca
+
+Tâches réalisées :
+  2023-10-11 (capl1101, sart0701, thea1804, turr3004) : Ajout des procédures et fonctions EMIR pour les stations.
+
+-- -----------------------------------------------------------------------------
+-- 3.4.0_SSQA_EMIR_Variable.sql
+-- =========================================================================== Z
+*/
