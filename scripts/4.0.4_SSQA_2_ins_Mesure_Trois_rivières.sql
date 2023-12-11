00d5c49dd8317f790a0706b6e867b9ce555a7f76
AlexTheisse <99749361+AlexTheisse@users.noreply.github.com>
2023-12-10 22:59:45 -0400

    [~] Add new mesure with EMIR

---
+++ scripts/4.0.4_SSQA_2_ins_Mesure_Trois_rivières.sql
@@ -0,0 +1,45 @@
+a-Madeleine,Sodium PST,2021-11-24,0.4,0.4,mesure
+/*
+04048 - Trois-Rivières - Cap-de-la-Madeleine,2022,2022-02-24 12:00,,,0.145,,,36.309,,,4.4043,0.0167
+04048 - Trois-Rivières - Cap-de-la-Madeleine,2022,2022-02-24 13:00,,,0.096,,,36.2242,,,4.8818,0.0838
+04048 - Trois-Rivières - Cap-de-la-Madeleine,2022,2022-02-24 14:00,,,0.3357,,,37.7458,,,4.5505,0.115
+04048 - Trois-Rivières - Cap-de-la-Madeleine,2022,2022-02-24 15:00,,,0.3057,,,37.0707,,,4.6505,0.107
+04048 - Trois-Rivières - Cap-de-la-Madeleine,2022,2022-02-24 16:00,,,0.1705,,,37.0498,,,5.3332,0.1362
+04048 - Trois-Rivières - Cap-de-la-Madeleine,2022,2022-02-24 17:00,,,0.1412,,,38.0048,,,5.0167,0.116
+04048 - Trois-Rivières - Cap-de-la-Madeleine,2022,2022-02-24 18:00,,,0.161,,,39.0913,,,5.4248,0.0788
+04048 - Trois-Rivières - Cap-de-la-Madeleine,2022,2022-02-24 19:00,,,0.1293,,,38.6675,,,4.5663,0.0573
+04048 - Trois-Rivières - Cap-de-la-Madeleine,2022,2022-02-24 20:00,,,0.1587,,,38.6437,,,7.5738,0.0412
+04048 - Trois-Rivières - Cap-de-la-Madeleine,2022,2022-02-24 21:00,,,0.3175,,,13.8202,,,35.1442,0.07
+04048 - Trois-Rivières - Cap-de-la-Madeleine,2022,2022-02-24 22:00,,,0.479,,,4.2527,,,42.7405,0.1008
+04048 - Trois-Rivières - Cap-de-la-Madeleine,2022,2022-02-24 23:00,,,0.5408,,,3.529,,,35.1857,0.1503
+04048 - Trois-Rivières - Cap-de-la-Madeleine,2022,2022-02-25 00:00,,,0.2605,,,13.7368,,,25.8793,0.0633
+04048 - Trois-Rivières - Cap-de-la-Madeleine,2022,2022-02-25 01:00,,,0.3438,,,15.4548,,,14.426,0.0836
+04048 - Trois-Rivières - Cap-de-la-Madeleine,2022,2022-02-25 02:00,,,0.2106,,,17.8387,,,14.0022,0.0338
+04048 - Trois-Rivières - Cap-de-la-Madeleine,2022,2022-02-25 03:00,,,0.3028,,,18.7438,,,10.5072,0.0258
+04048 - Trois-Rivières - Cap-de-la-Madeleine,2022,2022-02-25 04:00,,,0.2211,,,31.2526,,,7.306,0.0213
+04048 - Trois-Rivières - Cap-de-la-Madeleine,2022,2022-02-25 05:00,,,0.1182,,,29.9792,,,5.7327,0.0142
+*/
+
+--Ajout des mesures de 2021-01-04 à 2021-01-10 pour le site 04048 avec les fonctions EMIR du fichier 4.0.3.2
+
+mesure_ins_gen_sst_exs(04048, 'SO2', 2022-02-24 12:00, 0.0167 , 'B1');
+mesure_ins_gen_sst_exs(04048, 'SO2', 2022-02-24 13:00, 0.0838 , 'B1');
+mesure_ins_gen_sst_exs(04048, 'SO2', 2022-02-24 14:00, 0.115 , 'B1');
+mesure_ins_gen_sst_exs(04048, 'SO2', 2022-02-24 15:00, 0.107 , 'B1');
+mesure_ins_gen_sst_exs(04048, 'SO2', 2022-02-24 16:00, 0.1362 , 'B1');
+mesure_ins_gen_sst_exs(04048, 'SO2', 2022-02-24 17:00, 0.116 , 'B1');
+mesure_ins_gen_sst_exs(04048, 'SO2', 2022-02-24 18:00, 0.0788 , 'B1');
+mesure_ins_gen_sst_exs(04048, 'SO2', 2022-02-24 19:00, 0.0573 , 'B1');
+mesure_ins_gen_sst_exs(04048, 'SO2', 2022-02-24 20:00, 0.0412 , 'B1');
+mesure_ins_gen_sst_exs(04048, 'SO2', 2022-02-24 21:00, 0.07 , 'B1');
+mesure_ins_gen_sst_exs(04048, 'SO2', 2022-02-24 22:00, 0.1008 , 'B1');
+mesure_ins_gen_sst_exs(04048, 'SO2', 2022-02-24 23:00, 0.1503 , 'B1');
+mesure_ins_gen_sst_exs(04048, 'SO2', 2022-02-25 00:00, 0.0633 , 'B1');
+mesure_ins_gen_sst_exs(04048, 'SO2', 2022-02-25 01:00, 0.0836 , 'B1');
+mesure_ins_gen_sst_exs(04048, 'SO2', 2022-02-25 02:00, 0.0338 , 'B1');
+mesure_ins_gen_sst_exs(04048, 'SO2', 2022-02-25 03:00, 0.0258 , 'B1');
+mesure_ins_gen_sst_exs(04048, 'SO2', 2022-02-25 04:00, 0.0213 , 'B1');
+mesure_ins_gen_sst_exs(04048, 'SO2', 2022-02-25 05:00, 0.0142 , 'B1');
+
+
+    
